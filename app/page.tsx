973f623a2c0bda7231ef505b3d696685ca34f59c
tarik619 <itarik707@gmail.com>
2024-03-15 15:17:09 +0600

    adding logo on home page

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
-  return <div className="text-sky-500 text-3xl">Hello there! </div>;
-}
