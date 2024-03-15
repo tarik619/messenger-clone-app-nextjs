8ace45eef9ca650ddfcee75a89631120b9c8eb1f
tarik619 <itarik707@gmail.com>
2024-03-15 15:02:38 +0600

    small change to see everuthing working fine

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,5 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <>Hello there! </>;
+  return <div className="text-sky-500 text-3xl">Hello there! </div>;
 }
