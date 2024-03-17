b0358b726fa252ab5db55232091700bf21afe604
tarik619 <itarik707@gmail.com>
2024-03-17 16:30:27 +0600

    successfully added github and google login functionality

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -7,6 +7,8 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import axios from "axios";
+import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -38,16 +40,37 @@ const AuthForm = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
     if (variant === "REGISTER") {
-      axios.post("/api/register", data);
+      axios
+        .post("/api/register", data)
+        .catch(() => toast.error("something went wrong"))
+        .finally(() => setIsLoading(false));
     }
     if (variant === "LOGIN") {
-      // nextauth signin
+      signIn("credentials", { ...data, redirect: false })
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error("Invalid credentials");
+          }
+          if (callback?.ok && !callback?.error) {
+            toast.success("Loggged successful");
+          }
+        })
+        .finally(() => setIsLoading(false));
     }
   };
 
   const socialActions = (action: string) => {
     setIsLoading(true);
-    // nextauth social sign in
+    signIn(action, { redirect: false })
+      .then((callback) => {
+        if (callback?.error) {
+          toast.error("something went wrong");
+        }
+        if (callback?.ok && !callback?.error) {
+          toast.success("Loggged successful");
+        }
+      })
+      .finally(() => setIsLoading(false));
   };
 
   return (
