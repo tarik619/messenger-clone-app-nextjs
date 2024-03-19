3a5e215d100f7f32a4d9f31d993a3c268b120bd5
tarik619 <itarik707@gmail.com>
2024-03-19 16:08:15 +0600

    created new component in users file emptystate

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,12 +1,10 @@
-"use client";
-
-import { signOut } from "next-auth/react";
+import EmptyState from "../components/EmptyState";
 
 const Users = () => {
   return (
-    <button className="" onClick={() => signOut()}>
-      logout
-    </button>
+    <div className="hidden lg:block lg:pl-80 h-full">
+      <EmptyState />
+    </div>
   );
 };
 
